776ea4ee9311e05ecdbfc2077ea4b1c420a3f7d6
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2025-01-13 12:48:00 +0530

    First Commit

---
+++ src/projects/diceroll/dice-role-game.py
@@ -0,0 +1,15 @@
+#run the code
+import random
+from unittest.mock import right
+
+while True:
+  choice =input("Do you want to play the game (Y/N): ").lower()
+  if choice=='y':
+    dice1 =random.randint(1,6)
+    dice2= random.randint(1,6)
+    print(f"{dice1}, {dice2}")
+  elif choice=='n':
+    print("Thanks for playing.!")
+    break
+  else:
+    print("invalid choice")
